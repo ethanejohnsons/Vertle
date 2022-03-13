3506ddf72f98cbc5625dcd8b045d855bfbcfc154
ethanejohnsons <ethan@ejohnsons.com>
2022-03-13 00:33:32 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Vertle from './Vertle';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Vertle />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
