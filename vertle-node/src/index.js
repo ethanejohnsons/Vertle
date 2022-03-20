84b69a046db68eac845f02bc5c462f0e8370f9fb
ethanejohnsons <ethan@ejohnsons.com>
2022-03-20 18:05:19 -0500

    Added daily answer endpoint to node server

--- vertle-node/src/index.js
+++ vertle-node/src/index.js
@@ -1,14 +1,20 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+function getAnswerOfTheDay() {
+    return 0b111000000000000;
+}
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end('111000000000000');
+const server = app.listen(4000, () => {
+    const host = server.address().address;
+    const port = server.address().port
+    console.log("Vertle API listening at http://%s:%s", host, port);
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/daily', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.end(JSON.stringify({
+        answer: getAnswerOfTheDay().toString(2),
+        vertices: 6
+    }));
 });
