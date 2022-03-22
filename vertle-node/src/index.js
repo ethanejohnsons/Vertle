f27a173bfa8a2023e70f2aa19fce71eb7da84864
ethanejohnsons <ethan@ejohnsons.com>
2022-03-22 18:44:56 -0500

    Added helmet

--- vertle-node/src/index.js
+++ vertle-node/src/index.js
@@ -1,8 +1,41 @@
 const express = require('express');
+const fs = require('fs');
+const helmet = require('helmet');
+
 const app = express();
+app.use(helmet());
+
+const historyFile = 'history.json';
+let entryCache = null;
+
+function generateAnswerOfTheDay(n) {
+    return Math.floor(Math.random() * Math.pow(2, n));
+}
 
 function getAnswerOfTheDay() {
-    return 0b111000000000000;
+    let today = new Date().toISOString().slice(0, 10);
+
+    if (entryCache && entryCache.date === today.toString()) {
+        return entryCache;
+    } else {
+        let history = JSON.parse(fs.readFileSync(historyFile));
+
+        if (history.length > 0 && history[history.length - 1].date === today.toString()) {
+            entryCache = history[history.length - 1];
+        } else {
+            entryCache = {
+                gameNumber: history.length + 1,
+                date: today,
+                answer: generateAnswerOfTheDay(15).toString(2),
+                vertices: 6
+            };
+
+            history.push(entryCache);
+            fs.writeFileSync(historyFile, JSON.stringify(history));
+        }
+    }
+
+    return entryCache;
 }
 
 const server = app.listen(4000, () => {
@@ -13,8 +46,5 @@ const server = app.listen(4000, () => {
 
 app.get('/daily', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.end(JSON.stringify({
-        answer: getAnswerOfTheDay().toString(2),
-        vertices: 6
-    }));
+    res.end(JSON.stringify(getAnswerOfTheDay()));
 });
