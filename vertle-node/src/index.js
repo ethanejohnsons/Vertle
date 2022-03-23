c5315cee2fecd500057babb77803d57fbb5acbfc
ethanejohnsons <ethan@ejohnsons>
2022-03-23 00:30:58 +0000

    Nevermind, going back...

--- vertle-node/src/index.js
+++ vertle-node/src/index.js
@@ -44,7 +44,7 @@ const server = app.listen(4000, () => {
     console.log("Vertle API listening at http://%s:%s", host, port);
 });
 
-app.get('daily', (req, res) => {
+app.get('/daily', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.end(JSON.stringify(getAnswerOfTheDay()));
 });
